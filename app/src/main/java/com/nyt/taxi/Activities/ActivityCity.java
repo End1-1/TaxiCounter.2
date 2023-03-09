421ffac38a736f50a5d149e23fa6c69f1a73abe4
unknown <end1_1@yahoo.com>
2023-03-09 21:43:31 +0400

    Title bar not transparency

--- app/src/main/java/com/nyt/taxi/Activities/ActivityCity.java
+++ app/src/main/java/com/nyt/taxi/Activities/ActivityCity.java
@@ -275,19 +275,6 @@ public class ActivityCity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        WindowInsetsControllerCompat windowInsetsController =
-                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
-        windowInsetsController.setSystemBarsBehavior(
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        );
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
-//        getWindow().getDecorView().setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-//                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-//                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
 
